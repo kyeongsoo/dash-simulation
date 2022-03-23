aa628a576678d8f7b20b42948abaa39ea8de6044
Kyeong Soo (Joseph) Kim <kyeongsoo.kim@gmail.com>
2022-03-23 15:39:00 +0800

    Update for TensorFlow version 2.6.

--- predict_bw_lstm1.py
+++ predict_bw_lstm1.py
@@ -1,9 +1,20 @@
-# This code is based on the nice sample code from:
-# https://machinelearningmastery.com/how-to-develop-lstm-models-for-time-series-forecasting/
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+##
+# @file     predict_bw_lstm1.py
+# @author   Kyeong Soo (Joseph) Kim <Kyeongsoo.Kim@xjtlu.edu.cn>
+# @date     2019-04-22
+#           2022-03-23 - updated for TensorFlow version 2.6
+#
+# @brief    Predict channel bandwidth.
+#
+# @remarks  This code is based on the nice sample code from:
+#           https://machinelearningmastery.com/how-to-develop-lstm-models-for-time-series-forecasting/
+
+# import modules
 import numpy as np
-from keras.models import Sequential
-from keras.layers import LSTM
-from keras.layers import Dense
+import tensorflow as tf
+import tensorflow.keras         # required for TF ver. 2.6
 from skimage.util import view_as_windows
 
 # define dataset
@@ -15,9 +26,10 @@ y = bws[3:]
 X = X.reshape((X.shape[0], X.shape[1], 1))
 
 # define model
-model = Sequential()
-model.add(LSTM(50, activation='relu', input_shape=(3, 1)))
-model.add(Dense(1))
+model = tf.keras.Sequential()
+# model.add(tf.keras.layers.LSTM(units=50, activation='relu', input_shape=(3, 1)))
+model.add(tf.keras.layers.LSTM(units=50, activation='relu'))
+model.add(tf.keras.layers.Dense(1))
 model.compile(optimizer='adam', loss='mse')
 
 # fit model
@@ -28,4 +40,4 @@ for i in range(10):
     x_input = X[i]
     x_input = x_input.reshape((1, 3, 1))
     yhat = model.predict(x_input, verbose=0)
-    print('{0} -> {1:.2e} (true value: {2:d})'.format( ','.join([str(int(i)) for i in x_input.flatten()]), yhat.flatten()[0], int(y[i])))
+    print(f"{','.join([str(int(i)) for i in x_input.flatten()])} -> {yhat.flatten()[0]:.2e} (true value: {int(y[i]):d})")
